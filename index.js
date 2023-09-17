38e96b2c9b58e473507ca439a573b84a2e3773ee
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-17 03:07:39 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === "male") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  // Creating instances and calling the speak method
+  const cat = new Cat("Whiskers", "female");
+  console.log(cat.speak()); // Output: "Whiskers says meow!"
+  
+  const dog = new Dog("Fido", "male");
+  console.log(dog.speak()); // Output: "Fido says woof!"
+  
+  const bird = new Bird("Polly", "male");
+  console.log(bird.speak()); // Output: "It's me! Polly, the parrot!"
+  
+  const femaleBird = new Bird("Mia", "female");
+  console.log(femaleBird.speak()); // Output: "Mia says squawk!"
+  
